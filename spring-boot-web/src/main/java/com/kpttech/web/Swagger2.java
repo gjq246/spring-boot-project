abdcab49c140b540972ea696a68650267d9b1e86
gjq246 <30488907@qq.com>
2017-08-31 11:53:27 +0800

    swagger 认证登录

--- spring-boot-web/src/main/java/com/kpttech/web/Swagger2.java
+++ spring-boot-web/src/main/java/com/kpttech/web/Swagger2.java
@@ -3,6 +3,8 @@ package com.kpttech.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,23 +16,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2 {
-	
+
 	@Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kpttech.web.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("springboot利用swagger构建api文档")
-                .description("简单优雅的restfun风格，http://blog.csdn.net/gjq246")
-                .termsOfServiceUrl("http://blog.csdn.net/gjq246")
-                .version("1.0")
-                .build();
-    }
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.kpttech.web.controller"))
+				// .paths(PathSelectors.any())
+				.paths(Predicates.or(
+						// 这里添加你需要展示的接口
+						PathSelectors.ant("/user/**"), PathSelectors.ant("/wx/**")))
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("springboot利用swagger构建api文档")
+				.description("简单优雅的restfun风格，http://blog.csdn.net/gjq246")
+				.termsOfServiceUrl("http://blog.csdn.net/gjq246").version("1.0").build();
+	}
 
 }
